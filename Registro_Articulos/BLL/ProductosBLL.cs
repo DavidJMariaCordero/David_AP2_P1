2a62e7f79d94aa5607d3ca8291786d2d93d7aac1
David Maria <eldavid_46@hotmail.com>
2021-06-02 18:51:14 -0400

    Agregando la BLL de productos

---
+++ Registro_Articulos/BLL/ProductosBLL.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_Articulos.DAL;
+using Registro_Articulos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registro_Articulos.BLL
+{
+    public class ProductosBLL
+    {
+        public static bool Guardar(Productos productos)
+        {
+            if (!Existe(productos.ProductoId))
+                return Insertar(productos);
+            else
+                return Modificar(productos);
+        }
+
+        public static bool Insertar(Productos productos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Productos.Add(productos);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Modificar(Productos productos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(productos).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool eliminado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var productos = contexto.Productos.Find(id);
+                if(productos != null)
+                {
+                    contexto.Productos.Remove(productos);
+                    eliminado = contexto.SaveChanges() > 0;
+                }
+
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return eliminado;
+        }
+
+        public static Productos Buscar(int id)
+        {
+            
+            Contexto contexto = new Contexto();
+            Productos producto;
+
+            try
+            {
+                producto = contexto.Productos.Find(id);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return producto;
+        }
+
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+            try
+            {
+                encontrado = contexto.Productos.Any(p => p.ProductoId == id);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+
+        }
+    }
+}
