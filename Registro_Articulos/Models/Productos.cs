bdd5adbc2a191d60f96389188ec54f8bfdd73aa0
David Maria <eldavid_46@hotmail.com>
2021-06-02 18:26:09 -0400

    Agregando validaciones a la clase productos

--- Registro_Articulos/Models/Productos.cs
+++ Registro_Articulos/Models/Productos.cs
@@ -10,8 +10,14 @@ namespace Registro_Articulos.Models
     {
         [Key]
         public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "Debe introducir una descripcion")]
         public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Debe introducir la existencia del producto")]
         public double Existencia { get; set; }
+
+        [Required(ErrorMessage = "Debe introducir el costo del producto")]
         public double Costo { get; set; }
         public double ValorInventario { get; set; }
 
