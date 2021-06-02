e0caf23698b4aa42deda1caad691e416e1557c89
David Maria <eldavid_46@hotmail.com>
2021-06-02 18:22:47 -0400

    Agregando clase productos

---
+++ Registro_Articulos/Models/Productos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registro_Articulos.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public double Existencia { get; set; }
+        public double Costo { get; set; }
+        public double ValorInventario { get; set; }
+
+    }
+}
